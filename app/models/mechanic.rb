e86a9dce7f1fdb605e5fc703f7c9aaabf7f932e0
Wookeun Song <wookeun.s@gmail.com>
2019-09-22 22:40:46 -0500

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,30 @@
+require_relative 'car_owner'
+require_relative 'car'
+
 class Mechanic
 
   attr_reader :name, :specialty
+
+  @@all = []
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
 end
 
+def cars
+  Car.all.select { |car| car.classification == self.specialty}
+end
+
+def car_owners
+  all_cars = cars.map { |car| car.owner}
+  all_cars.select { |car| car.owner}
+end
+
+# CLASS METHODS BELOW
+
+def self.all
+  @@all
+end
 
 end
